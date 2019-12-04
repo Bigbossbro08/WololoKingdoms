e57a4a55eda5813ad169cdfeebc23f18846fed2b
Jineapple <Jineapple@users.noreply.github.com>
2019-12-04 20:45:42 +0100

    Change IDs of new units

--- libwololokingdoms/fixes/addunits.cpp
+++ libwololokingdoms/fixes/addunits.cpp
@@ -18,14 +18,16 @@ void addRoamingWolf(genie::DatFile * df) {
   // ID for graphics for the roaming wolf.
   const size_t graphicsIdWolfxWn = 1392;
 
+  // ID for the new unit
+  const size_t unitIdRoamingWolf = 1310;
+
   auto unit = df->Civs[civIdGaia].Units[unitIdWolf];
   unit.LanguageDLLName = langFileNameRoamingWolf;
   unit.LanguageDLLCreation = langFileNameRoamingWolf + 1000;
   unit.LanguageDLLHelp = langFileNameRoamingWolf + 100000;
 
-  auto headerId = df->UnitHeaders.size();
-  df->UnitHeaders.push_back(df->UnitHeaders[unitIdWolf]);
-  auto taskList = &df->UnitHeaders[headerId].TaskList;
+  df->UnitHeaders[unitIdRoamingWolf] = df->UnitHeaders[unitIdWolf];
+  auto taskList = &df->UnitHeaders[unitIdRoamingWolf].TaskList;
   auto taskFly = genie::Task();
   taskFly.IsDefault = true;
   taskFly.ActionType = genie::Task::ActionTypes::Fly;
@@ -44,13 +46,12 @@ void addRoamingWolf(genie::DatFile * df) {
   }
 
   //id fix
-  unit.ID = headerId;
-  unit.CopyID = headerId;
-  unit.BaseID = headerId;
+  unit.ID = unitIdRoamingWolf;
+  unit.CopyID = unitIdRoamingWolf;
+  unit.BaseID = unitIdRoamingWolf;
 
   for (auto& civ : df->Civs) {
-    civ.Units.push_back(unit);
-    civ.UnitPointers.push_back(1);
+    civ.Units[unitIdRoamingWolf] = unit;
   }
 }
 
@@ -69,6 +70,9 @@ void addTransportSharkatzor(genie::DatFile * df) {
   // Garrison capacity for the custom Transport Sharkatzor unit.
   const size_t sharkatzorTransportGarrisonCapacity = 10;
 
+  // ID for the new unit
+  const size_t unitIdSharkatzorTransport = 1311;
+
   auto unit = df->Civs[civIdGaia].Units[unitIdSharkatzor];
   unit.Class = genie::Unit::UnitClass::TransportBoat;
   unit.ResourceCapacity = sharkatzorTransportResourceCapacity;
@@ -80,9 +84,8 @@ void addTransportSharkatzor(genie::DatFile * df) {
   unit.LanguageDLLCreation = 6655;
   unit.LanguageDLLHelp = 105655;
 
-  auto headerId = df->UnitHeaders.size();
-  df->UnitHeaders.push_back(df->UnitHeaders[unitIdSharkatzor]);
-  auto taskList = &df->UnitHeaders[headerId].TaskList;
+  df->UnitHeaders[unitIdSharkatzorTransport] = df->UnitHeaders[unitIdSharkatzor];
+  auto taskList = &df->UnitHeaders[unitIdSharkatzorTransport].TaskList;
 
   // Removes the tasks at indices 0 and 2.
   taskList->erase(taskList->begin());
@@ -98,13 +101,12 @@ void addTransportSharkatzor(genie::DatFile * df) {
   }
 
   // id fix
-  unit.ID = headerId;
-  unit.CopyID = headerId;
-  unit.BaseID = headerId;
+  unit.ID = unitIdSharkatzorTransport;
+  unit.CopyID = unitIdSharkatzorTransport;
+  unit.BaseID = unitIdSharkatzorTransport;
 
   for (auto& civ : df->Civs) {
-    civ.Units.push_back(unit);
-    civ.UnitPointers.push_back(1);
+    civ.Units[unitIdSharkatzorTransport] = unit;
   }
 }
 
@@ -117,58 +119,91 @@ void addUnitSpawner(genie::DatFile * df) {
   // Unit ID for the Llama building.
   const size_t unitIdLlamaBuilding = 888;
 
+  // Unit Id for the first unit of the spawners
+  const size_t spawnerUnitIdStart = 1312;
+
   auto unitA = df->Civs[civIdGaia].Units[unitIdLlamaBuilding];
   unitA.DeadUnitID = -1;
   unitA.Building.TechID = -1; // Sets "Initiates Technology".
-  auto unitIdA = df->Civs[civIdGaia].Units.size();
+  unitA.ID = spawnerUnitIdStart;
 
   auto unitB = unitA;
-  auto unitIdB = unitIdA + 1;
-  unitA.Building.StackUnitID = unitIdB;
+  unitB.ID = unitA.ID + 1;
+
+  unitB.HideInEditor = 0;
+  unitA.Building.StackUnitID = unitB.ID;
 
   auto unitC = unitA;
-  unitC.Building.HeadUnit = unitIdA;
+  unitC.ID = unitA.ID + 2;
+  unitC.Building.HeadUnit = unitA.ID;
   auto annexC = genie::unit::BuildingAnnex();
-  annexC.UnitID = unitIdA + 2;
+  annexC.UnitID = unitA.ID + 2;
   annexC.Misplacement = {0.5f, -0.5f};
 
   auto unitD = unitA;
-  unitD.Building.HeadUnit = unitIdA;
+  unitD.ID = unitA.ID + 3;
+  unitD.Building.HeadUnit = unitA.ID;
   auto annexD = genie::unit::BuildingAnnex();
-  annexD.UnitID = unitIdA + 3;
+  annexD.UnitID = unitA.ID + 3;
   annexD.Misplacement = {-0.5f, 0.5f};
 
   auto unitE = unitA;
-  unitE.Building.HeadUnit = unitIdA;
+  unitE.ID = unitA.ID + 4;
+  unitE.Building.HeadUnit = unitA.ID;
   auto annexE = genie::unit::BuildingAnnex();
-  annexE.UnitID = unitIdA + 4;
+  annexE.UnitID = unitA.ID + 4;
   annexE.Misplacement = {0.5f, 0.5f};
 
   auto unitF = unitA;
-  unitF.Building.HeadUnit = unitIdA;
+  unitF.ID = unitA.ID + 5;
+  unitF.Building.HeadUnit = unitA.ID;
   auto annexF = genie::unit::BuildingAnnex();
-  annexF.UnitID = unitIdA + 5;
+  annexF.UnitID = unitA.ID + 5;
   annexF.Misplacement = {-0.5f, -0.5f};
 
   std::vector<genie::unit::BuildingAnnex> annexes = {annexC, annexD, annexE,
                                                      annexF};
   unitA.Building.Annexes = annexes;
-  int langId = 5039;
   auto units = {unitA, unitB, unitC, unitD, unitE, unitF};
+
+  int langId = 5639;
   for (auto unit : units) {
-    df->UnitHeaders.push_back(df->UnitHeaders[unitIdLlamaBuilding]);
-    unit.ID = df->Civs[0].Units.size();
-    unit.CopyID = unit.ID;
-    unit.BaseID = unit.ID;
+    df->UnitHeaders[unit.ID] = df->UnitHeaders[unitIdLlamaBuilding];
     unit.LanguageDLLName = langId;
     unit.LanguageDLLCreation = langId + 1000;
     unit.LanguageDLLHelp = langId + 100000;
+    unit.CopyID = unit.ID;
+    unit.BaseID = unit.ID;
     ++langId;
     for (auto& civ : df->Civs) {
-      civ.Units.push_back(unit);
-      civ.UnitPointers.push_back(1);
+      civ.Units[unit.ID] = unit;
     }
   }
+}
+
+// Copies a number of units in the 900-1000 ID range in `df` to a safer range.
+// @param df the dat file to modify
+void copy900IdUnits(genie::DatFile* df) {
+
+  // First unit ID in the range to copy
+  const size_t originalId = 900;
+
+  // First ID in a safe range
+  const size_t newId = 1318;
+
+  // Number of consecutive units to copy
+  const size_t unitCountToCopy = 50;
+
+  for (size_t i = 0; i < unitCountToCopy; i++) {
+    df->UnitHeaders[newId + i] = df->UnitHeaders[originalId + i];
+    for (auto& civ : df->Civs) {
+      civ.Units[newId + i] = civ.Units[originalId + i];
+      civ.Units[newId + i].ID = newId + i;
+      civ.Units[newId + i].CopyID = newId + i;
+      civ.Units[newId + i].BaseID = newId + i;
+    }
+  }
+
 }
 
 // Adds several new units:
@@ -179,6 +214,7 @@ void addUnitsPatch(genie::DatFile* aocDat) {
   addRoamingWolf(aocDat);
   addTransportSharkatzor(aocDat);
   addUnitSpawner(aocDat);
+  copy900IdUnits(aocDat);
 }
 
 DatPatch addUnits = {&addUnitsPatch, "Adds various additional units."};
